53bfb55b2dfe050fd6aae1b8c8e8cbc48006a8fe
decollozq <decollzoq@gmail.com>
2025-09-23 13:56:05 +0900

    refactor: profileImage를 Header에 병합 및 피드백 반영

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,19 @@
 import styled from '@emotion/styled'
-import ProfileImage from './img/ProfileImage'
+import { Color } from 'src/models/color'
+
+const PROFILE_IMAGE = 'https://i.ifh.cc/mpapcv.jpg'
+
+const ProfileImageWrapper = styled.img`
+  width: 44px;
+  height: 44px;
+  border-radius: 50%;
+`
 
 export default function Header() {
   return (
     <HeaderWrapper>
-      <ProfileImage />
-      <Title>박민선</Title>
+      <ProfileImageWrapper src={PROFILE_IMAGE} alt='Profile Iamge' />
+      <Title style={{ marginTop: '12px', marginLeft: '12px' }}>박민선</Title>
     </HeaderWrapper>
   )
 }
@@ -21,12 +29,13 @@ const HeaderWrapper = styled.header`
   height: 77px;
   padding: 12px 10px 12px 28px;
   margin: 0 0 0 0;
-  background-color: #fff;
+  background-color: ${Color.background};
 `
 
-const Title = styled.div`
-  margin-top: 12px;
-  margin-left: 16px;
+export const Title = styled.div`
+  text-align: left;
   font-size: 20px;
-  font-weight: 700;
+  font-weight: bold;
+  color: ${Color.gray100};
+  width: fit-content;
 `
