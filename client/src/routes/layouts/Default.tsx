49eb2638f34a7d5ac245e9a5062b39aee0c3124c
decollozq <decollzoq@gmail.com>
2026-04-16 20:09:35 +0900

    Refactor: 기존 footer 태그를 삭제하고, 기본 레이아웃에 Footer 컴포넌트 추가

--- client/src/routes/layouts/Default.tsx
+++ client/src/routes/layouts/Default.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router";
 import { ScrollRestoration } from "react-router";
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
 export default function DefaultLayout() {
     return (
         <>
             <Header />
             <Outlet />
             <ScrollRestoration />
+            <Footer />
         </>
     );
 }
