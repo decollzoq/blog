8db8658e081c4bf135ec737bb3f2b8e8390c389a
decollozq <decollzoq@gmail.com>
2026-04-20 14:04:45 +0900

    Refactor: CategoryProvider 사용하여 Home.tsx 에서 CategoryContext 사용할 수 있도록 함

--- client/src/routes/layouts/Default.tsx
+++ client/src/routes/layouts/Default.tsx
@@ -2,12 +2,15 @@ import { Outlet } from "react-router";
 import { ScrollRestoration } from "react-router";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import { CategoryProvider } from "../../contexts/providers/CategoryProvider";
 
 export default function DefaultLayout() {
     return (
         <>
             <Header />
-            <Outlet />
+            <CategoryProvider>
+                <Outlet />
+            </CategoryProvider>
             <ScrollRestoration />
             <Footer />
         </>
