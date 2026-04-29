5c146c79c19d6926f0bb59a351e46f7220a627af
decollozq <decollzoq@gmail.com>
2026-04-29 18:02:53 +0900

    Feature : DefferedComponent 도입 및 페이지별 스켈레톤 UI 구현 - 200ms 지연 로직을 통한 로딩 깜빡임 방지 - DefaultLayout 내 Suspense 적용

--- client/src/routes/layouts/Default.tsx
+++ client/src/routes/layouts/Default.tsx
@@ -3,7 +3,15 @@ import { ScrollRestoration } from "react-router";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import { CategoryProvider } from "../../contexts/providers/CategoryProvider";
+import { Suspense } from "react";
+import DefferedComponent from "../../components/DefferedComponent";
+import Home from "../pages/Home";
+import PostDetails from "../pages/PostDetails";
 
+import {
+    HomeLoadingSkeleton,
+    PostDetailSkeleton,
+} from "../../components/LoadingSkeleton";
 export default function DefaultLayout() {
     return (
         <>
@@ -16,3 +24,23 @@ export default function DefaultLayout() {
         </>
     );
 }
+
+<Suspense
+    fallback={
+        <DefferedComponent>
+            <HomeLoadingSkeleton />
+        </DefferedComponent>
+    }
+>
+    <Home />
+</Suspense>;
+
+<Suspense
+    fallback={
+        <DefferedComponent>
+            <PostDetailSkeleton />
+        </DefferedComponent>
+    }
+>
+    <PostDetails />
+</Suspense>;
