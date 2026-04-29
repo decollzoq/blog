c9d35c5348134a544d3475d0e09e1b9fc0f07a32
decollozq <decollzoq@gmail.com>
2026-04-29 18:01:14 +0900

    Refactor: 라우팅 식별자를 postId에서 slug로 변경

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
                 element: <Home />,
             },
             {
-                path: "/posts/:postId",
+                path: "/posts/:slug",
                 element: <PostDetails />,
             },
         ],
